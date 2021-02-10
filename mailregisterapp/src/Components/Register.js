53be305370b1fbf8f5fa26660bfd79c516fc6515
sibasisb <sibasisbhattacharjee@gmail.com>
2021-02-10 12:16:56 +0530

    added mailregisterapp

--- mailregisterapp/src/Components/Register.js
+++ mailregisterapp/src/Components/Register.js
@@ -1,6 +1,30 @@
 import React,{Component} from 'react'
 import '../Stylesheets/mystyles.css'
 
+const validateForm=(state)=>{
+    let nameMsg="",emailMsg="",passwordMsg="";
+    if(state.name=="" || state.name.length<5){
+        nameMsg="Name should have at least 5 characters";
+    }
+    const regex=/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(!regex.test(state.email)){
+        emailMsg="Email is invalid";      
+    }
+    if(state.password=="" || state.password.length<8){
+        passwordMsg="Password should have at least 8 characters";
+    }
+
+    const res={
+        fullName:nameMsg,
+        email:emailMsg,
+        password:passwordMsg
+    }
+
+    console.log(res);
+
+    return res;
+}
+
 export class Register extends Component{
 
     constructor(props){
@@ -8,8 +32,13 @@ export class Register extends Component{
         this.state={
             name:"",
             email:"",
-            password:""
-        }
+            password:"",
+            errors:{
+                fullName:"",
+                email:"",
+                password:""
+            }
+        };
         this.handleSubmit=this.handleSubmit.bind(this);
         this.handleNameChange=this.handleNameChange.bind(this);
         this.handleEmailChange=this.handleEmailChange.bind(this);
@@ -19,30 +48,59 @@ export class Register extends Component{
     handleNameChange(event){
         this.setState(state=>({
             name:event.target.value
-        }))
+        }));
     }
 
     handleEmailChange(event){
         this.setState(state=>({
             email:event.target.value
-        }))
+        }));
     }
 
     handlePasswordChange(event){
         this.setState(state=>({
             password:event.target.value
-        }))
+        }));
     }
+
+    
 
     handleSubmit(e){
         e.preventDefault();
-        const cname=this.state.name;
-        const ctid=this.state.tid;
-        alert(" Thanks  " + cname + "\n Your complaint was submitted" + "\n Your transaction ID is " + ctid);
+        const res=validateForm(this.state);
         this.setState(state=>({
-            name:"",
-            complaint:""
-        }));
+            errors:res
+        }),
+        ()=>{
+            if(res.fullName=="" && res.email=="" && res.password==""){
+                alert("Valid Form");
+                this.setState(state=>({
+                    name:"",
+                    email:"",
+                    password:"",
+                    errors:{
+                        fullName:"",
+                        email:"",
+                        password:""
+                    }
+                }));
+            }
+            else {
+                let str="";
+                if(this.state.errors.fullName!==""){
+                    str=str + this.state.errors.fullName;
+                }
+                if(this.state.errors.email!==""){
+                    str=str + "\n" + this.state.errors.email;
+                }
+                if(this.state.errors.password!==""){
+                    str=str + "\n" + this.state.errors.password + "\n";
+                }
+                alert(str);
+            }
+            
+        });
+        
     }
 
 
